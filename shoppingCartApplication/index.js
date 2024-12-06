3d2dbbd3dc9d8d97787677c43c63f54ef865ef5f
jicob <68771461+jicobret@users.noreply.github.com>
2024-12-06 08:08:47 +0100

    Update index.js

--- shoppingCartApplication/index.js
+++ shoppingCartApplication/index.js
@@ -5,13 +5,13 @@ class Cart {
 
     addProduct({ name, price, quantity = 1 }) {
         const existingProductIndex = this.products.findIndex(product => product.name === name); // zwraca -1 jesli produkt nie istnieje
-        if (existingProductIndex !== -1) { // sprawdza czy produkt juz istnieje bazujac na wyzszej metodzie findIndex, jezeli tak to zwieksza jego ilosc o podana wartosc
+        if (existingProductIndex !== -1) { 
             this.products[existingProductIndex] = {
                 ...this.products[existingProductIndex],
                 quantity: this.products[existingProductIndex].quantity + quantity
             };
         } else {
-            this.products = [...this.products, { name, price, quantity }]; // dodaje produkt jako nowy w przypadku nie wystepowania go w koszyku
+            this.products = [...this.products, { name, price, quantity }];
         }
         console.log(`Dodano: ${name}, cena: ${price}, ilość: ${quantity}`);
     }
